38a51e5d3c19a345eea9b0592825e2d7a99353b0
Steve Alden <stevealden.dev@gmail.com>
2025-04-17 22:14:12 +0530

    chore: Add all shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+'use client';
+import { Preview } from '@react-email/components';
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      Hey
+      <Email />
+    </>
+  );
 }
+
+const Email = () => {
+  return <Preview>Email preview text</Preview>;
+};
